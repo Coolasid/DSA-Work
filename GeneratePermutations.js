ed1136be351ec34404b854f52fc52b4fac38253b
Siddesh Patil <87792589+Coolasid@users.noreply.github.com>
2021-10-21 13:56:57 -0400

    21-10

---
+++ GeneratePermutations.js
@@ -0,0 +1,26 @@
+function runProgram(input) {
+   var input = input.trim().split("\n");
+   var arr
+   
+  }
+  if (process.env.USERNAME === "siddhesh") {
+      runProgram(`3
+1 2 3`);
+  } else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+      read += input;
+    });
+    process.stdin.on("end", function () {
+      read = read.replace(/\n$/, "");
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+    });
+    process.on("SIGINT", function () {
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+      process.exit(0) ;
+    });
+  }
