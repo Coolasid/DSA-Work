92607df1922f09fd534e9499fa8441d8270361b8
Siddesh Patil <87792589+Coolasid@users.noreply.github.com>
2021-12-05 00:11:35 -0500

    5-12

--- Masai Packers & Movers II.js
+++ Masai Packers & Movers II.js
@@ -1,57 +1,53 @@
-let count = 0;
-
-function masaiways(arr, N, K) {
+var count = 0;
+var arr1 = [];
+function masaiways2(arr, N, K){
     
-    if (K < 0) {
-        return 0;
-    }
     if(K == 0){
         count++;
+       
         return;
     }
-    
 
- 
-
-    for(var i = 0; i < N; i++){
-        masaiways(arr, N, K - arr[i]);
+    if(K < 0){
+        return;
+    }
+    for(var i = 0; i < arr.length; i++){
+        masaiways2(arr,N, K - arr[i])
     }
 }
-
-
 
 function runProgram(input) {
     var input = input.trim().split("\n");
     var arr1 = input[0].trim().split(" ").map(Number);
     var arr = input[1].trim().split(" ").map(Number);
-   
+
     var K = +arr1[0];
     var N = +arr1[1];
-    
-    masaiways(arr, N, K);
+
+    masaiways2(arr, N, K);
     console.log(count)
-   
+console.log(arr1);
 
 
 }
-if (process.env.USERNAME === "siddhesh") {
-    runProgram(`26 4
-9 6 7 10`);
-} else {
+  if (process.env.USERNAME === "siddhesh") {
+      runProgram(`3 3
+1 2 3`);
+  } else {
     process.stdin.resume();
     process.stdin.setEncoding("ascii");
     let read = "";
     process.stdin.on("data", function (input) {
-        read += input;
+      read += input;
     });
     process.stdin.on("end", function () {
-        read = read.replace(/\n$/, "");
-        read = read.replace(/\n$/, "");
-        runProgram(read);
+      read = read.replace(/\n$/, "");
+      read = read.replace(/\n$/, "");
+      runProgram(read);
     });
     process.on("SIGINT", function () {
-        read = read.replace(/\n$/, "");
-        runProgram(read);
-        process.exit(0);
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+      process.exit(0) ;
     });
-}
+  }
