be3b3ea973b5e53ec96c7f310f17f71d087f5241
Siddesh Patil <87792589+Coolasid@users.noreply.github.com>
2021-12-10 21:15:22 -0500

    10-12

--- Array Equation.js
+++ Array Equation.js
@@ -1,39 +1,37 @@
+function arrEqu(N, arr){
+
+    var sumE = 0;
+    var sumO = 0;
+    for(var i = 0; i < N; i++){
+
+        if(arr[i] % 2 == 0){
+            sumE += arr[i]
+        }else{
+            sumO += arr[i];
+        }
+
+        
+
+    }
+
+    console.log((2*sumE + 3*sumO))
 
 
-function allSub(str,newStr,curr){
-    // return N
-    
-  if(newStr.length  > 0){
-    // var ans = newStr.join()
-    console.log(newStr.join(" "));
- 
-
-    
-  }
-  if(curr == str.length){
-    return;
-  }
-
-  for(var i = curr; i < str.length; i++){
-    newStr.push(str[i]);
-    allSub(str,newStr,i+1);
-    newStr.pop();
-  }
-  
 }
 
 
 function runProgram(input) {
    var input = input.trim().split("\n");
+
    var N = +input[0];
-   var str = input[1].trim().split(" ").map(Number)
-   var curr = 0;
-  var newStr = [];
-   allSub(str,newStr,curr);
+   var arr = input[1].trim().split(" ").map(Number);
+
+    arrEqu(N,arr);
+   
   }
   if (process.env.USERNAME === "siddhesh") {
-      runProgram(`4
-1 2 3 4`);
+      runProgram(`5
+1 2 3 4 5`);
   } else {
     process.stdin.resume();
     process.stdin.setEncoding("ascii");
