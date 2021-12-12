a281f158529b4255a10583240a6ab57958cd222f
Siddesh Patil <87792589+Coolasid@users.noreply.github.com>
2021-12-12 15:22:40 -0500

    12-12

--- Counting Occurences of Digits 2.js
+++ Counting Occurences of Digits 2.js
@@ -1,105 +1,48 @@
-function cODigits(N, arr){
+function countOccD(N, arr){
     // console.log(N,arr);
 
-    var obj = {};
-
-    for (var i = 0; i < arr.length; i++) {
-        if (obj[arr[i]] === undefined) {
-            obj[arr[i]] = 1;
-        } else {
-            obj[arr[i]]++;
-        }
-
+    var obj = {
+        0:0,
+        1:0,
+        2:0,
+        3:0,
+        4:0,
+        5:0,
+        6:0,
+        7:0,
+        8:0,
+        9:0
     }
 
-    // console.log(obj)
+    for(var i = 0; i < N; i++){
+        obj[arr[i]]++;
+    }
+    var ans = [];
+    // console.log(obj);
 
-    var z = [];
-    var n = [];
-    for( key in obj){
-        z.push(obj[key]);
-    }
-    for(var i = z.length-1; i>= 0; i--){
-        n.push(z[i]);
-    }
-    // console.log(n);
-    // console.log(z)
     for(key in obj){
-        // console.log(obj[key]);
-
-   while(i >= 9){
-
-    
-        if(key== "9"){
-            var a = obj[key];
-        }else{
-            a = 0;
-        }
-        if (key == "8") {
-            var b = obj[key];
-        } else {
-            b = 0;
-        }
-        if (key == "7") {
-            var c = obj[key];
-        } else {
-            c = 0;
-        }
-        if (key == "6") {
-            var d = obj[key];
-        } else {
-            d = 0;
-        }
-        if (key == "5") {
-            var e = obj[key];
-        } else {
-            e = 0;
-        }
-        if (key == "4") {
-            var f = obj[key];
-        } else {
-            f = 0;
-        }
-        if (key == "3") {
-            var g = obj[key];
-            // console.log(obj[key]);
-        } else {
-            g = 0;
-        }
-        if (key == "2") {
-            var h = obj[key];
-        } else {
-            h = 0;
-        }
-        if (key == "1") {
-            var i = obj[key];
-        } else {
-            i = 0;
-        }
-        if (key == "0") {
-            var j = obj[key];
-        } else {
-            j = 0;
-        }
-        
+        ans.push(obj[key]);
     }
-}
-    console.log(a, b, c, d, e, f, g, h, i, j);
+
+    console.log(ans.reverse().join(" "))
+    
+
     
 }
 
 
 function runProgram(input) {
    var input = input.trim().split("\n");
-    var testCases = +input[0];
+   var testCase = +input[0];
 
    var line = 1;
-   for(var i = 0; i< testCases; i++){
+   for(var i = 0; i < testCase; i++){
        var N = +input[line];
        line++;
        var arr = input[line].trim().split("").map(Number);
-        line++;
-       cODigits(N,arr);
+       line++;
+
+       countOccD(N, arr);
    }
    
   }
