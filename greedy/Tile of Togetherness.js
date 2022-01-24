9db4498a794d44c0e5e482afef4f9df1ff049451
Siddesh Patil <87792589+Coolasid@users.noreply.github.com>
2022-01-24 10:53:24 +0530

    24-1

--- greedy/Tile of Togetherness.js
+++ greedy/Tile of Togetherness.js
@@ -1,68 +1,47 @@
-function tileOfT(R, H, arr){
-arr.push(H);
-  for(var i = 0; i< arr.length; i++){
 
-      if( arr[i] == R){
-      return R;
-
-      }
-    }
-
-    if( R > H){
-      return -1;
-    }
-    
-    // console.log(arr);
-
-    
-    
-    let toS = R.toString();
-
-    let toS1 = H.toString();
- 
-    let s = 0;
-
-    let s1 = 0;
-
-    for(var i = 0; i < toS.length; i++){
-
-        s += (+toS[i]);
-
-    }
-
-    for(var i = 0; i < toS1.length; i++){
-
-      s1 += (+toS1[i]);
-
-    } 
-
-    
-      return  tileOfT( (R + s), (H + s1), arr);
-    
-    
-  }
-
+function tileOfT(R, H){
   
+  myset = new Set();
+  myset.add(H);
+  
+  while(R <= H){
+    if( myset.has(R)) return R;
 
+    let temp = R.toString().split("").map(Number).reduce(sum, 0);
+    let temp2 = H.toString().split("").map(Number).reduce(sum, 0);
+    
+    // console.log(temp2);
 
+    R = R + temp;
+    H = H + temp2;
+    
+    myset.add(H);
+ 
+  }
+ return -1;
+    
+}
+function sum(a, b){
 
+  return a + b;
 
+}
 function runProgram(input) {
    var [A, B] = input.trim().split(" ").map(Number);
     let R,H
 //    console.log(A, B);
 
     if( A < B){
-         R = A;
+        R = A;
         H = B
     }else{
         R = B;
         H = A;
     }
     
-    var arr = []
+    
 
-   console.log( tileOfT(R, H, arr));
+   console.log( tileOfT(R, H));
    
   }
   if (process.env.USERNAME === "siddhesh") {
